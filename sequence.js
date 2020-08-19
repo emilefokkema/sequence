410f61598597ebb5ce9abb1c73cd9e4c16366c14
Emile Fokkema <emilefokkema@gmail.com>
2020-08-19 17:39:46 +0200

    add recurse

--- sequence.js
+++ sequence.js
@@ -32,6 +32,23 @@
 			};
 		};
 	};
+	var filter = function f(s, predicate){
+		return function(){
+			var first, rest = s;
+			do{
+				if(!rest){
+					return null;
+				}
+				var result = rest();
+				first = result.first;
+				rest = result.rest;
+			}while(!predicate(first))
+			return {
+				first: first,
+				rest: f(rest, predicate)
+			};
+		};
+	};
 	var zip = function z(s, t, mapFn){
 		if(!s){
 			return null;
@@ -49,6 +66,10 @@
 				rest: z(_s.rest, otherRest, mapFn)
 			};
 		};
+	};
+	var recurse = function r(s, fn){
+		var _s = s();
+		return fn(_s.first, function(){return r(_s.rest, fn);});
 	};
 	var take = function t(s, n){
 		if(n === 0 || !s){
@@ -76,27 +97,21 @@
 	};
 
 	var integers = reduce(stream, x => x + 1, 0);
-	var zipped = zip(integers, integers, function(item, other){
-		if(item % 3 === 0){
-			return other();
-		}
-		return item;
-	});
-	console.log(toArray(zipped, 0, 10));
+	
 
-	// var isJuf = function(getal){
-	// 	if(getal % 7 === 0){
-	// 		return true;
-	// 	}
-	// 	while(getal > 0){
-	// 		var laatste = getal % 10;
-	// 		if(laatste > 0 && laatste % 7 === 0){
-	// 			return true;
-	// 		}
-	// 		getal = (getal - laatste) / 10;
-	// 	}
-	// 	return false;
-	// };
+	var isJuf = function(getal){
+		if(getal % 7 === 0){
+			return true;
+		}
+		while(getal > 0){
+			var laatste = getal % 10;
+			if(laatste > 0 && laatste % 7 === 0){
+				return true;
+			}
+			getal = (getal - laatste) / 10;
+		}
+		return false;
+	};
 	// var gaTellen = reduce(stream, x => x + 1, 0);
 
 	// var inceptionJuffen = inception(gaTellen, isJuf);
@@ -115,13 +130,42 @@
 		skip(n){
 			return new Stream(skip(this.fn, n));
 		}
+		zip(s, mapFn){
+			return new Stream(zip(this.fn, s.fn, mapFn));
+		}
 		take(n){
 			return new Stream(take(this.fn, n));
+		}
+		filter(predicate){
+			return new Stream(filter(this.fn, predicate));
 		}
 		toArray(startIndex, endIndex){
 			return toArray(this.fn, startIndex, endIndex);
 		}
+		recurse(fn){
+			return recurse(this.fn, fn);
+		}
+		static proxy(getOther){
+			return new Stream(() => getOther().fn());
+		}
 	}
 	
+	var integers = new Stream().reduce(x => x + 1, 0);
+	var inceptionJuffen = integers
+		.reduce((_, i) => 
+			integers.reduce((_, j) => 
+				({value: j, levels: [j]})))
+		.recurse((level, getNextLevel) =>
+			level.zip(Stream.proxy(getNextLevel), function(integerOnLevel, getIntegerOneLevelUp){
+				if(isJuf(integerOnLevel.value)){
+					var oneLevelUp = getIntegerOneLevelUp();
+					return {value: oneLevelUp.value, levels: integerOnLevel.levels.concat(oneLevelUp.levels)};
+				}
+				return integerOnLevel;
+			}))
+		.reduce((_, x) => ({value: x.value, levels: x.levels.slice(0, x.levels.length - 1)}))
+	;
+	
+	console.log(JSON.stringify(inceptionJuffen.filter(x => x.value === 1).toArray(0, 9), null, 1));
 })();
 
